e4923ea9eab2ab1ea7e343d8bc4eaa21e68be5e0
Adnan <sirgeant10@gmail.com>
2024-09-23 15:11:14 +0600

    Added toggle to content and btn

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const donationCards = document.getElementById("donation-cards");
+const donationHistory = document.getElementById("donation-history");
+const donationBtn = document.getElementById("donation-btn");
+const donationHistoryBtn = document.getElementById("donation-history-btn");
+
+donationBtn.addEventListener("click", (e) =>
+  toggleBtn(e.target.id, "donation-cards")
+);
+donationHistoryBtn.addEventListener("click", (e) =>
+  toggleBtn(e.target.id, "donation-history")
+);
