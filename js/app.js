31981dea31136bd1dcfa5a8ca5185887d82c8d8c
Adnan <sirgeant10@gmail.com>
2024-09-23 15:35:03 +0600

    Added donation amount from functions

--- js/app.js
+++ js/app.js
@@ -1,9 +1,28 @@
+// Buttons
 const donationBtn = document.getElementById("donation-btn");
 const donationHistoryBtn = document.getElementById("donation-history-btn");
+// Forms
+const noakhaliForm = document.getElementById("noakhali-form");
+const feniForm = document.getElementById("feni-form");
+const quotaForm = document.getElementById("quota-form");
 
+// Button Events
 donationBtn.addEventListener("click", (e) =>
   toggleBtn(e.target.id, "donation-cards")
 );
 donationHistoryBtn.addEventListener("click", (e) =>
   toggleBtn(e.target.id, "donation-history")
+);
+
+// Form Events
+noakhaliForm.addEventListener("submit", (e) =>
+  addToDonations(e, "noakhali-amount-input", "noakhali-amount")
+);
+
+feniForm.addEventListener("submit", (e) =>
+  addToDonations(e, "feni-amount-input", "feni-amount")
+);
+
+quotaForm.addEventListener("submit", (e) =>
+  addToDonations(e, "quota-amount-input", "quota-amount")
 );
--- js/reusables.js
+++ js/reusables.js
@@ -1,3 +1,4 @@
+// toggle btn and Toggle Content
 function toggleBtn(id, sectionId) {
   // Toggle Btn
   document.getElementById("donation-btn").classList.remove("bg-[#B4F461]");
@@ -10,4 +11,17 @@ function toggleBtn(id, sectionId) {
   document.getElementById("donation-history").classList.add("hidden");
   document.getElementById("donation-cards").classList.add("hidden");
   document.getElementById(sectionId).classList.remove("hidden");
+}
+
+// Add to donations
+function addToDonations(e, input, donated) {
+  e.preventDefault();
+
+  let inputAmount = document.getElementById(input);
+  const donatedAmount = document.getElementById(donated);
+
+  donatedAmount.innerText =
+    parseFloat(donatedAmount.innerText) + parseFloat(inputAmount.value);
+
+  inputAmount.value = "";
 }
